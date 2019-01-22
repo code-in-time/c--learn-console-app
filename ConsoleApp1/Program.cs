7fb51446c7d8c92a4c07b97553d33d21bedc9065
Andrew Crawford <code-in-time@hotmail.com>
2019-01-22 21:35:39 +0200

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStopWatch s = new Analogue("wwwwww");
+            s.StartTimer();
+        }
+
+    }
+}
---
+++ ConsoleApp1/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    abstract class StopWatch : IStopWatch
+    {
+        string _name = null;
+        Int64 _counter = 0;
+
+        public StopWatch(string name)
+        {
+            Console.Write("watch");
+            _name = name;
+        }
+
+        public void StartTimer()
+        {
+            Console.WriteLine("StartTimer {0}", _name);
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += (sender, e) => OnTimedEvent(sender, e, this);
+            aTimer.Interval = 1000;
+            aTimer.Enabled = true;
+
+
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e, StopWatch stopWatch)
+        {
+            Console.WriteLine("Hello World!", source);
+            Console.WriteLine("v", stopWatch._counter);
+            stopWatch._counter += 1;
+            Console.WriteLine("_counter {0}", stopWatch._counter);
+        }
+    }
+}
